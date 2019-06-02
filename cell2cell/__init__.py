38369bc4824874ba71791fa6b0f2ce6eded84068
earmingol <earmingol14@gmail.com>
2019-06-02 00:03:24 -0700

    Parallel computing functions

--- cell2cell/__init__.py
+++ cell2cell/__init__.py
@@ -5,5 +5,6 @@ from __future__ import absolute_import
 import cell2cell.analysis
 import cell2cell.clustering
 import cell2cell.core
+import cell2cell.extras
 import cell2cell.io
 import cell2cell.preprocessing
