c3b2ccbfb7b178d3efcce71ba54063e09372ff43
earmingol <earmingol14@gmail.com>
2019-06-02 00:01:08 -0700

    Changes in parallel computing

--- cell2cell/analysis/subsampling.py
+++ cell2cell/analysis/subsampling.py
@@ -7,20 +7,26 @@ import pandas as pd
 import random
 
 from cell2cell.core import InteractionSpace
-#from cell2cell.clustering import clustering_interactions
-from multiprocessing import Pool, cpu_count
+from cell2cell.extras import parallel_computing
 from contextlib import closing
+from multiprocessing import Pool
 
 class SubsamplingSpace:
 
-    def __init__(self, rnaseq_data, ppi_dict, interaction_type, gene_cutoffs, subsampling_percentage=0.8, iterations=1000, n_jobs=1, verbose=True):
+    def __init__(self, rnaseq_data, ppi_dict, interaction_type, gene_cutoffs, subsampling_percentage=0.8,
+                 iterations=1000, n_jobs=1, verbose=True):
+
         if verbose:
-            print("Computing interactions by subsampling a {}% of cells in each of the {} iterations".format(int(100*subsampling_percentage), iterations))
+            print("Computing interactions by subsampling a"
+                  " {}% of cells in each of the {} iterations".format(int(100*subsampling_percentage), iterations))
+
         self.cell_ids = list(rnaseq_data.columns)
 
-        self.cci_matrix_template = pd.DataFrame(np.zeros((len(self.cell_ids), len(self.cell_ids))),
-                                                         columns=self.cell_ids,
-                                                         index=self.cell_ids)
+        data = np.empty((len(self.cell_ids), len(self.cell_ids)))
+        data[:] = np.nan
+        self.cci_matrix_template = pd.DataFrame(data,
+                                                columns=self.cell_ids,
+                                                index=self.cell_ids)
 
         self.subsampled_interactions = self.subsampling_interactions(rnaseq_data=rnaseq_data,
                                                                      ppi_dict=ppi_dict,
@@ -32,59 +38,42 @@ class SubsamplingSpace:
                                                                      verbose=verbose)
 
 
-    def subsampling_interactions(self, rnaseq_data, ppi_dict, interaction_type, gene_cutoffs, subsampling_percentage=0.8, iterations=1000, n_jobs=1, verbose=True):
+    def subsampling_interactions(self, rnaseq_data, ppi_dict, interaction_type, gene_cutoffs,
+                                 subsampling_percentage=0.8, iterations=1000, n_jobs=1, verbose=True):
         '''
         This function performs the subsampling method by generating a list of cells to consider in each iteration.
         Then, for each list of cells a InteractionSpace is generated to perform the cell2cell anaysis and return the
         respective CCI Matrix and clusters.
         '''
-        # Defining number of cores
-        if n_jobs < 0:
-            agents = cpu_count() + 1 + n_jobs
-            if agents < 0:
-                agents = 1
-        elif n_jobs > cpu_count():
-            agents = cpu_count()
-
-        elif n_jobs == 0:
-            agents = 1
-        else:
-            agents = n_jobs
-
         # Last position for subsampling when shuffling
         last_item = int(len(self.cell_ids) * subsampling_percentage)
 
-        inputs = [self.cell_ids,
-                  last_item,
-                  rnaseq_data,
-                  ppi_dict,
-                  interaction_type,
-                  gene_cutoffs,
-                  self.cci_matrix_template,
-                  verbose
-                  ]
+        inputs = {'cells' : self.cell_ids,
+                  'list_end' : last_item,
+                  'rnaseq' : rnaseq_data,
+                  'ppi' : ppi_dict,
+                  'interaction_type' : interaction_type,
+                  'cutoffs' : gene_cutoffs,
+                  'cci_matrix' : self.cci_matrix_template,
+                  'verbose' :verbose
+                  }
 
         inputs = [inputs]*iterations
 
-        chunksize=1
+
+        # Parallel computing
+        agents = parallel_computing.agents_number(n_jobs)
+        chunksize = 1
         with closing(Pool(processes=agents)) as pool:
-            results = pool.map(subsampling_unit, inputs, chunksize)
+            results = pool.map(parallel_computing.parallel_subsampling_interactions, inputs, chunksize)
         return results
 
 
-def subsampling_unit(inputs):
+def subsampling_operation(cell_ids, last_item, rnaseq_data, ppi_dict, interaction_type, gene_cutoffs,
+                          cci_matrix_template = None, verbose=True):
     '''
     Functional unit to perform parallel computing in SubsamplingSpace
     '''
-    cell_ids = inputs[0]
-    last_item = inputs[1]
-    rnaseq_data = inputs[2]
-    ppi_dict = inputs[3]
-    interaction_type = inputs[4]
-    gene_cutoffs = inputs[5]
-    cci_matrix_template = inputs[6]
-    verbose = inputs[7]
-
     random.shuffle(cell_ids)
     included_cells = cell_ids[:last_item]
 
@@ -97,9 +86,7 @@ def subsampling_unit(inputs):
 
     interaction_space.compute_pairwise_interactions(verbose=verbose)
 
-    # clustering
-    # clusters = clustering_interactions(interaction_space.interaction_elements['cci_matrix'],
-    #                                   method='louvain')
-
-    iteration_elements = (interaction_space.interaction_elements['cci_matrix'])  # , clusters)
+    iteration_elements = dict()
+    iteration_elements['cells'] = included_cells
+    iteration_elements['cci_matrix'] = interaction_space.interaction_elements['cci_matrix']
     return iteration_elements
