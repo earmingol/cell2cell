5c1ac48da16a11c0519168fb9addc7658527dae2
earmingol <earmingol14@gmail.com>
2019-06-18 16:39:35 -0700

    Refactor of gene ontology

--- cell2cell/preprocessing/__init__.py
+++ cell2cell/preprocessing/__init__.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 
 from cell2cell.preprocessing.integrate_data import *
 from cell2cell.preprocessing.cutoffs import *
-from cell2cell.preprocessing.geneontology import *
+from cell2cell.preprocessing.gene_ontology import *
 from cell2cell.preprocessing.integrate_data import *
 from cell2cell.preprocessing.ppi import *
 from cell2cell.preprocessing.rnaseq import *
