27a345f62ee1e90829bf4cc4a10f98d3aab44caa
earmingol <earmingol14@gmail.com>
2019-06-24 21:46:34 -0700

    Parallel Leiden

--- cell2cell/extras/parallel_computing.py
+++ cell2cell/extras/parallel_computing.py
@@ -6,7 +6,7 @@ from cell2cell.analysis import subsampling
 from cell2cell.clustering import cluster_interactions
 from multiprocessing import cpu_count
 
-
+# GENERAL
 def agents_number(n_jobs):
     '''
     This function computes the number of available agents based on the number of jobs provided.
@@ -25,7 +25,7 @@ def agents_number(n_jobs):
         agents = n_jobs
     return agents
 
-
+# CORE FUNCTIONS
 def parallel_subsampling_interactions(inputs):
     '''
     Parallel computing in cell2cell2.subsampling.SubsamplingSpace
@@ -44,6 +44,7 @@ def parallel_subsampling_interactions(inputs):
     return results
 
 
+# CLUSTERING FUNCTIONS
 def parallel_community_detection(inputs):
     '''
     Parallel computing in cell2cell2.clustering.clustering_interactions
@@ -53,5 +54,18 @@ def parallel_community_detection(inputs):
 
     results = cluster_interactions.community_detection(cci_matrix=cci_matrix,
                                                        seed=inputs['seed'],
+                                                       package=inputs['package'],
                                                        verbose=inputs['verbose'])
+    return results
+
+
+def parallel_leiden_community(inputs):
+    '''
+    Parallel computing in cell2cell2.clustering.clustering_interactions
+    '''
+    included_cells = inputs['interaction_elements']['cells']
+    cci_matrix = inputs['interaction_elements']['cci_matrix'].loc[included_cells, included_cells]
+
+    results = cluster_interactions.leiden_community(cci_matrix=cci_matrix,
+                                                    verbose=inputs['verbose'])
     return results
