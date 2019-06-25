c590836efbf78a6b68f1d65ad51fdb1bd439f596
earmingol <earmingol14@gmail.com>
2019-06-24 21:47:40 -0700

    Added global percentile

--- cell2cell/preprocessing/cutoffs.py
+++ cell2cell/preprocessing/cutoffs.py
@@ -10,10 +10,13 @@ import pandas as pd
 
 def local_percentile_cutoff(rnaseq_data, percentile = 0.8):
     cutoffs = rnaseq_data.quantile(percentile, axis=1).to_frame()
-    #cutoffs.index = rnaseq_data.index
     cutoffs.columns = ['value']
     return cutoffs
 
+def global_percentile_cutoff(rnaseq_data, percentile = 0.8):
+    cutoffs = pd.DataFrame(index=rnaseq_data.index, columns=['value'])
+    cutoffs['value'] = np.quantile(rnaseq_data.values, percentile)
+    return cutoffs
 
 def standep(rnaseq_data, k):
     '''Not implemented yet'''
@@ -27,6 +30,8 @@ def get_cutoffs(rnaseq_data, parameters, verbose=True):
         print("Calculating cutoffs for gene abundances")
     if type == 'local_percentile':
         cutoffs = local_percentile_cutoff(rnaseq_data, parameter)
+    elif type == 'global_percentile':
+        cutoffs = global_percentile_cutoff(rnaseq_data, parameter)
     elif type == 'standep':
         cutoffs = standep(rnaseq_data, parameter)
     elif type == 'file':
