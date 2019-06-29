c0a0aaa8bf0834f59e5692c89c415b6da4fa548e
earmingol <earmingol14@gmail.com>
2019-06-29 15:03:08 -0700

    Added weighted ppi and rna-seq (each gene divided by max val) to generate a continuous value for complementarity index.

--- cell2cell/preprocessing/integrate_data.py
+++ cell2cell/preprocessing/integrate_data.py
@@ -3,12 +3,15 @@
 from __future__ import absolute_import
 
 import pandas as pd
+import cell2cell.preprocessing as preprocessing
 from cell2cell.preprocessing import ppi, gene_ontology
 
 ## RNAseq data
 def get_modified_rnaseq(rnaseq_data, function_type='binary', **kwargs):
     if function_type == 'binary':
         modified_rnaseq = get_binary_rnaseq(rnaseq_data, kwargs['cutoffs'])
+    elif function_type == 'sample_weighted':
+        modified_rnaseq = preprocessing.divide_expression_by_max(rnaseq_data, axis=1)
     else:
         # As other functions are implemented, other elif condition will be included here.
         raise NotImplementedError("Function type {} to compute pairwise cell-interactions is not implemented".format(function_type))
@@ -17,8 +20,8 @@ def get_modified_rnaseq(rnaseq_data, function_type='binary', **kwargs):
 
 def get_binary_rnaseq(rnaseq_data, cutoffs):
     binary_rnaseq_data = rnaseq_data.copy()
-    for column in rnaseq_data.columns:
-        binary_rnaseq_data[column] = binary_rnaseq_data[column].to_frame().apply(axis=1, func=lambda row: 1.0 if (row[column] >= cutoffs.loc[row.name, 'value']) else 0.0).values
+    binary_rnaseq_data = binary_rnaseq_data.ge(list(cutoffs.value.values), axis=0)
+    binary_rnaseq_data= binary_rnaseq_data.astype(float)
     return binary_rnaseq_data
 
 
@@ -26,6 +29,8 @@ def get_binary_rnaseq(rnaseq_data, cutoffs):
 def get_weighted_ppi(ppi_data, modified_rnaseq_data, column='value', function_type='binary'):
     if function_type == 'binary':
         weighted_ppi = get_binary_ppi(ppi_data, modified_rnaseq_data, column)
+    elif function_type == 'sample_weighted':
+        weighted_ppi = get_sample_weighted_ppi(ppi_data, modified_rnaseq_data, column)
     else:
         # As other functions are implemented, other elif condition will be included here.
         raise NotImplementedError("Function type {} to compute pairwise cell-interactions is not implemented".format(function_type))
@@ -33,14 +38,19 @@ def get_weighted_ppi(ppi_data, modified_rnaseq_data, column='value', function_ty
 
 
 def get_binary_ppi(ppi_data, binary_rnaseq_data, column='value'):
-    #binary_ppi = pd.DataFrame(columns=ppi_data.columns)
     binary_ppi = ppi_data.copy()
     binary_ppi['A'] = binary_ppi['A'].apply(func=lambda row: binary_rnaseq_data.loc[row, column])
-    binary_ppi = binary_ppi[['A', 'score']].reset_index(drop=True)
-
-    # binary_ppi['B'] = binary_ppi['B'].apply(func=lambda row: binary_rnaseq_data.loc[row, column])
-    # binary_ppi = binary_ppi[['A', 'B', 'score']].reset_index(drop=True)
+    binary_ppi['B'] = binary_ppi['B'].apply(func=lambda row: binary_rnaseq_data.loc[row, column])
+    binary_ppi = binary_ppi[['A', 'B', 'score']].reset_index(drop=True)
     return binary_ppi
+
+
+def get_sample_weighted_ppi(ppi_data, modified_rnaseq_data, column='value'):
+    sample_weighted_ppi = ppi_data.copy()
+    sample_weighted_ppi['A'] = sample_weighted_ppi['A'].apply(func=lambda row: modified_rnaseq_data.loc[row, column])
+    sample_weighted_ppi['B'] = sample_weighted_ppi['B'].apply(func=lambda row: modified_rnaseq_data.loc[row, column])
+    sample_weighted_ppi = sample_weighted_ppi[['A', 'B', 'score']].reset_index(drop=True)
+    return sample_weighted_ppi
 
 
 def ppis_from_proteins(ppi_data, contact_proteins, mediator_proteins=None, interaction_columns=['A', 'B']):
@@ -63,19 +73,11 @@ def ppis_from_proteins(ppi_data, contact_proteins, mediator_proteins=None, inter
                                                   mediator_proteins=mediator_proteins,
                                                   interaction_type='combined',
                                                   interaction_columns=interaction_columns)
-    # else:
-    #     columns = interaction_columns + ['score']
-    #     all_ppis['mediated'] = pd.DataFrame(columns=columns)
-    #     all_ppis['combined'] = pd.DataFrame(columns=columns)
-    # This is omitted because other functions use all_ppis.values() instead of checking all types of interactions.
     return all_ppis
 
 
 def ppis_from_goterms(ppi_data, go_annotations, go_terms, contact_go_terms, mediator_go_terms=None, use_children=True,
                       go_header='GO', gene_header='Gene', interaction_columns=['A', 'B'], verbose=True):
-
-
-
 
     if use_children == True:
         contact_proteins = gene_ontology.get_genes_from_parent_go_terms(go_annotations,
