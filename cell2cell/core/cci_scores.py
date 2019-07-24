41808f1d371f767399b48459406137694820cbb3
earmingol <earmingol14@gmail.com>
2019-07-24 14:41:02 -0700

    Unified jaccard & added correlation score

--- cell2cell/core/cci_scores.py
+++ cell2cell/core/cci_scores.py
@@ -5,46 +5,10 @@ from __future__ import absolute_import
 import numpy as np
 
 
-def compute_cci_score_from_binary(cell1, cell2):
+def compute_jaccard_like_cci_score(cell1, cell2):
     '''
-    Function that calculates an score for the interaction between two cells based on the interactions of their
-    proteins with the proteins of the other cell. This score is based on ........
-
-    Parameters
-    ----------
-    cell1 : Cell class
-        First cell/tissue/organ type to compute interaction between a pair of them.
-
-    cell2 : Cell class
-        Second cell/tissue/organ type to compute interaction between a pair of them.
-
-    Returns
-    -------
-    cci_score : float
-        Score for the interaction of the of the pair of cells based on the presence of gene/proteins in the ppi network.
-    '''
-    c1 = cell1.weighted_ppi['A'].values
-    c2 = cell2.weighted_ppi['B'].values
-
-    numerator = np.nansum(c1 * c2)
-    denominator = np.nansum(c1) + np.nansum(c2) - numerator
-
-    if denominator == 0.0:
-        return 0.0
-
-    cci_score = numerator / denominator
-
-    if cci_score is np.nan:
-        return 0.0
-
-    return cci_score
-
-
-def compute_cci_score_from_weighted(cell1, cell2):
-    '''
-    Function that calculates an score for the interaction between two cells based on the interactions of their
-    proteins with the proteins of the other cell. This score is based on the amount of each protein (expression level)
-    rather than its presence or absence (binary level).
+    Function that calculates an extended jaccard-like score for the interaction between two cells based on the
+    interactions of their proteins with the proteins of the other cell.
 
     Parameters
     ----------
@@ -74,5 +38,20 @@ def compute_cci_score_from_weighted(cell1, cell2):
 
     if cci_score is np.nan:
         return 0.0
+    return cci_score
 
+
+def compute_correlation_cci_score(cell1, cell2):
+    '''
+    This function
+    '''
+    from scipy.spatial.distance import correlation
+
+    c1 = cell1.weighted_ppi['A'].values
+    c2 = cell2.weighted_ppi['B'].values
+
+    cci_score = 1 - correlation(c1, c2)
+
+    if cci_score is np.nan:
+        return 0.0
     return cci_score
