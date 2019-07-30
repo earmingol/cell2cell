bce7196ec73644ec609239bc66fd73edf1c40aac
earmingol <earmingol14@gmail.com>
2019-07-29 23:06:38 -0700

    Replaced Dice score with Bray Curtis

--- cell2cell/core/cci_scores.py
+++ cell2cell/core/cci_scores.py
@@ -27,6 +27,9 @@ def compute_jaccard_like_cci_score(cell1, cell2):
     c1 = cell1.weighted_ppi['A'].values
     c2 = cell2.weighted_ppi['B'].values
 
+    if (len(c1) == 0) or (len(c2) == 0):
+        return 0.0
+
     # Extended Jaccard similarity
     numerator = np.nansum(c1 * c2)
     denominator = np.nansum(c1 * c1) + np.nansum(c2 * c2) - numerator
@@ -41,36 +44,7 @@ def compute_jaccard_like_cci_score(cell1, cell2):
     return cci_score
 
 
-def compute_dice_cci_score(cell1, cell2):
-    '''
-    Function that calculates an extended jaccard-like score for the interaction between two cells based on the
-    interactions of their proteins with the proteins of the other cell.
-
-    Parameters
-    ----------
-    cell1 : Cell class
-        First cell/tissue/organ type to compute interaction between a pair of them.
-
-    cell2 : Cell class
-        Second cell/tissue/organ type to compute interaction between a pair of them.
-
-    Returns
-    -------
-    cci_score : float
-        Score for the interaction of the of the pair of cells based on the abundance of gene/proteins in the ppi network.
-    '''
-    from scipy.spatial.distance import dice
-    c1 = cell1.weighted_ppi['A'].values
-    c2 = cell2.weighted_ppi['B'].values
-
-    cci_score = 1.0 - dice(c1, c2)
-
-    if cci_score is np.nan:
-        return 0.0
-    return cci_score
-
-
-def compute_braycurtis_cci_score(cell1, cell2):
+def compute_braycurtis_like_cci_score(cell1, cell2):
     '''
     Function that calculates an extended jaccard-like score for the interaction between two cells based on the
     interactions of their proteins with the proteins of the other cell.
@@ -92,6 +66,9 @@ def compute_braycurtis_cci_score(cell1, cell2):
     c1 = cell1.weighted_ppi['A'].values
     c2 = cell2.weighted_ppi['B'].values
 
+    if (len(c1) == 0) or (len(c2) == 0):
+        return 0.0
+
     cci_score = 1.0 - braycurtis(c1, c2)
 
     if cci_score is np.nan:
@@ -107,6 +84,9 @@ def compute_correlation_cci_score(cell1, cell2):
 
     c1 = cell1.weighted_ppi['A'].values
     c2 = cell2.weighted_ppi['B'].values
+
+    if (len(c1) == 0) or (len(c2) == 0):
+        return 0.0
 
     cci_score = 1.0 - correlation(c1, c2)
 
