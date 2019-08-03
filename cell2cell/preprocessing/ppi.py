64dfa30af74de0d7d6c44bc31e1f3cfba89f9aa3
earmingol <earmingol14@gmail.com>
2019-08-03 02:22:46 -0700

    Added complete interactions

--- cell2cell/preprocessing/ppi.py
+++ cell2cell/preprocessing/ppi.py
@@ -57,6 +57,9 @@ def filter_ppi_by_proteins(ppi_data, proteins):
 def bidirectional_ppi_for_cci(ppi_data, interaction_columns=['A', 'B'], verbose=True):
     if verbose:
         print("Making bidirectional PPI for CCI.")
+    if ppi_data.shape[0] == 0:
+        return ppi_data.copy()
+
     ppi_A = ppi_data.copy()
     col1 = ppi_A[[interaction_columns[0]]]
     col2 = ppi_A[[interaction_columns[1]]]
@@ -92,13 +95,19 @@ def filter_ppi_network(ppi_data, contact_proteins, mediator_proteins=None, refer
     header_interactorA = interaction_columns[0]
     header_interactorB = interaction_columns[1]
 
-    # Contact-Contact interactions
-    contacts = ppi_data.loc[ppi_data[header_interactorA].isin(contact_proteins) & ppi_data[header_interactorB].isin(contact_proteins)]
     if verbose:
         print('Filtering PPI interactions by using a list of genes for {} interactions'.format(interaction_type))
     if interaction_type == 'contacts':
+        # Contact-Contact interactions
+        contacts = ppi_data.loc[ppi_data[header_interactorA].isin(contact_proteins) & ppi_data[header_interactorB].isin(contact_proteins)]
         new_ppi_data = contacts.drop_duplicates()
+    elif interaction_type == 'complete':
+        total_prots = list(set(contact_proteins + mediator_proteins))
+        complete = ppi_data.loc[ppi_data[header_interactorA].isin(total_prots) & ppi_data[header_interactorB].isin(total_prots)]
+        new_ppi_data = complete.drop_duplicates()
     else:
+        # Contact-Contact interactions
+        contacts = ppi_data.loc[ppi_data[header_interactorA].isin(contact_proteins) & ppi_data[header_interactorB].isin(contact_proteins)]
         # Extracell-surface interactions
         mediated1 = ppi_data.loc[ppi_data[header_interactorA].isin(contact_proteins) & ppi_data[header_interactorB].isin(mediator_proteins)]
         mediated2 = ppi_data.loc[ppi_data[header_interactorA].isin(mediator_proteins) & ppi_data[header_interactorB].isin(contact_proteins)]
