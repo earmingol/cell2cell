ea04303de28a5bd88415cbb9bd94be536a8ca35e
earmingol <earmingol14@gmail.com>
2019-08-06 11:48:31 -0700

    Added complete interaction

--- cell2cell/preprocessing/integrate_data.py
+++ cell2cell/preprocessing/integrate_data.py
@@ -11,7 +11,7 @@ def get_modified_rnaseq(rnaseq_data, score_type='binary', **kwargs):
     if score_type == 'binary':
         modified_rnaseq = get_binary_rnaseq(rnaseq_data, kwargs['cutoffs'])
     elif score_type == 'absolute':
-        modified_rnaseq = rnaseq_data.copy()
+        modified_rnaseq = preprocessing.divide_expression_by_max(rnaseq_data)
     else:
         # As other score types are implemented, other elif condition will be included here.
         raise NotImplementedError("Score type {} to compute pairwise cell-interactions is not implemented".format(score_type))
@@ -30,30 +30,39 @@ def get_weighted_ppi(ppi_data, modified_rnaseq_data, column='value'):
     weighted_ppi = ppi_data.copy()
     weighted_ppi['A'] = weighted_ppi['A'].apply(func=lambda row: modified_rnaseq_data.loc[row, column])
     weighted_ppi['B'] = weighted_ppi['B'].apply(func=lambda row: modified_rnaseq_data.loc[row, column])
-    weighted_ppi = weighted_ppi[['A', 'B', 'score']].reset_index(drop=True)
+    weighted_ppi = weighted_ppi[['A', 'B', 'score']].reset_index(drop=True).fillna(0.0)
     return weighted_ppi
 
 
-def get_ppi_dict_from_proteins(ppi_data, contact_proteins, mediator_proteins=None, interaction_columns=['A', 'B']):
-
+def get_ppi_dict_from_proteins(ppi_data, contact_proteins, mediator_proteins=None, interaction_columns=['A', 'B'], verbose=True):
     ppi_dict = dict()
-    ppi_dict['contacts'] = ppi.filter_ppi_network(ppi_data,
-                                              contact_proteins,
-                                              mediator_proteins=mediator_proteins,
-                                              interaction_type='contacts',
-                                              interaction_columns=interaction_columns)
+    ppi_dict['contacts'] = ppi.filter_ppi_network(ppi_data=ppi_data,
+                                                  contact_proteins=contact_proteins,
+                                                  mediator_proteins=mediator_proteins,
+                                                  interaction_type='contacts',
+                                                  interaction_columns=interaction_columns,
+                                                  verbose=verbose)
     if mediator_proteins is not None:
-        ppi_dict['mediated'] = ppi.filter_ppi_network(ppi_data,
-                                                      contact_proteins,
+        ppi_dict['mediated'] = ppi.filter_ppi_network(ppi_data=ppi_data,
+                                                      contact_proteins=contact_proteins,
                                                       mediator_proteins=mediator_proteins,
                                                       interaction_type='mediated',
-                                                      interaction_columns=interaction_columns)
+                                                      interaction_columns=interaction_columns,
+                                                      verbose=verbose)
 
-        ppi_dict['combined'] = ppi.filter_ppi_network(ppi_data,
-                                                      contact_proteins,
+        ppi_dict['combined'] = ppi.filter_ppi_network(ppi_data=ppi_data,
+                                                      contact_proteins=contact_proteins,
                                                       mediator_proteins=mediator_proteins,
                                                       interaction_type='combined',
-                                                      interaction_columns=interaction_columns)
+                                                      interaction_columns=interaction_columns,
+                                                      verbose=verbose)
+
+        ppi_dict['complete'] = ppi.filter_ppi_network(ppi_data=ppi_data,
+                                                      contact_proteins=contact_proteins,
+                                                      mediator_proteins=mediator_proteins,
+                                                      interaction_type='complete',
+                                                      interaction_columns=interaction_columns,
+                                                      verbose=verbose)
     return ppi_dict
 
 
@@ -61,36 +70,37 @@ def get_ppi_dict_from_go_terms(ppi_data, go_annotations, go_terms, contact_go_te
                                go_header='GO', gene_header='Gene', interaction_columns=['A', 'B'], verbose=True):
 
     if use_children == True:
-        contact_proteins = gene_ontology.get_genes_from_go_hierarchy(go_annotations,
-                                                                     go_terms,
-                                                                     contact_go_terms,
+        contact_proteins = gene_ontology.get_genes_from_go_hierarchy(go_annotations=go_annotations,
+                                                                     go_terms=go_terms,
+                                                                     go_filter=contact_go_terms,
                                                                      go_header=go_header,
                                                                      gene_header=gene_header,
                                                                      verbose=verbose)
 
-        mediator_proteins = gene_ontology.get_genes_from_go_hierarchy(go_annotations,
-                                                                      go_terms,
-                                                                      mediator_go_terms,
+        mediator_proteins = gene_ontology.get_genes_from_go_hierarchy(go_annotations=go_annotations,
+                                                                      go_terms=go_terms,
+                                                                      go_filter=mediator_go_terms,
                                                                       go_header=go_header,
                                                                       gene_header=gene_header,
                                                                       verbose=verbose)
     else:
-        contact_proteins = gene_ontology.get_genes_from_go_terms(go_annotations,
-                                                                 contact_go_terms,
+        contact_proteins = gene_ontology.get_genes_from_go_terms(go_annotations=go_annotations,
+                                                                 go_filter=contact_go_terms,
                                                                  go_header=go_header,
                                                                  gene_header=gene_header)
 
-        mediator_proteins = gene_ontology.get_genes_from_go_terms(go_annotations,
-                                                                  mediator_go_terms,
+        mediator_proteins = gene_ontology.get_genes_from_go_terms(go_annotations=go_annotations,
+                                                                  go_filter=mediator_go_terms,
                                                                   go_header=go_header,
                                                                   gene_header=gene_header)
 
     # Avoid same genes in list
-    contact_proteins = list(set(contact_proteins) - set(mediator_proteins))
+    #contact_proteins = list(set(contact_proteins) - set(mediator_proteins))
 
-    ppi_dict = get_ppi_dict_from_proteins(ppi_data,
-                                          contact_proteins,
-                                          mediator_proteins,
-                                          interaction_columns=interaction_columns)
+    ppi_dict = get_ppi_dict_from_proteins(ppi_data=ppi_data,
+                                          contact_proteins=contact_proteins,
+                                          mediator_proteins=mediator_proteins,
+                                          interaction_columns=interaction_columns,
+                                          verbose=verbose)
 
     return ppi_dict
