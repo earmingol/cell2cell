d2e21a9c903901520f1e14e08c839a3eda135aff
earmingol <earmingol14@gmail.com>
2019-08-12 14:55:34 -0700

    CCI scores

--- cell2cell/core/cci_scores.py
+++ cell2cell/core/cci_scores.py
@@ -3,7 +3,7 @@
 from __future__ import absolute_import
 
 import numpy as np
-
+import math
 
 def compute_jaccard_like_cci_score(cell1, cell2):
     '''
@@ -81,26 +81,7 @@ def compute_braycurtis_like_cci_score(cell1, cell2):
     return cci_score
 
 
-def compute_correlation_cci_score(cell1, cell2):
-    '''
-    This function
-    '''
-    from scipy.spatial.distance import correlation
-
-    c1 = cell1.weighted_ppi['A'].values
-    c2 = cell2.weighted_ppi['B'].values
-
-    if (len(c1) == 0) or (len(c2) == 0):
-        return 0.0
-
-    cci_score = 1.0 - correlation(c1, c2)
-
-    if cci_score is np.nan:
-        return 0.0
-    return cci_score
-
-
-def compute_dot_score(cell1, cell2):
+def compute_dot_product_score(cell1, cell2):
     '''
     Function that calculates an dot score for the interaction between two cells based on the
     interactions of their proteins with the proteins of the other cell.
@@ -131,7 +112,7 @@ def compute_dot_score(cell1, cell2):
     return cci_score
 
 
-def compute_relative_potential_cci_score(cell1, cell2):
+def compute_sigmoid_cci_score(cell1, cell2):
     '''
     Function that calculates ...
 
@@ -148,23 +129,18 @@ def compute_relative_potential_cci_score(cell1, cell2):
     cci_score : float
         Score for the interaction of the of the pair of cells based on the abundance of gene/proteins in the ppi network.
     '''
+
+    def sigmoid(x):
+        return 1.0 / (1.0 + math.exp(-x))
+
+    dot_prod = compute_dot_product_score(cell1, cell2)
+
     c1 = cell1.weighted_ppi['A'].values
     c2 = cell2.weighted_ppi['B'].values
-
     if (len(c1) == 0) or (len(c2) == 0):
         return 0.0
 
-    c1 = c1.reshape((len(c1), 1))
-    c2 = c2.reshape((len(c2), 1))
-
-    numerator = np.nansum(c1 * c2)
-    potential = np.max(np.concatenate([c1, c2], axis=1), axis=1)
-    denominator = np.nansum(potential * potential)
-
-    if denominator == 0.0:
-        return 0.0
-
-    cci_score = numerator / denominator
+    cci_score = sigmoid(dot_prod) / float(len(c1))
 
     if cci_score is np.nan:
         return 0.0
