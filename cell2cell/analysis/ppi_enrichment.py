ec7f6cfd13881c82d34e6f50db5acd9ce08c7e14
earmingol <earmingol14@gmail.com>
2019-08-19 18:36:53 -0700

    Added interaction matrix

--- cell2cell/analysis/ppi_enrichment.py
+++ cell2cell/analysis/ppi_enrichment.py
@@ -8,6 +8,16 @@ import numpy as np
 import pandas as pd
 
 
+def ppi_count_operation(pair, subsampled_interactions):
+    cell1 = pair[0]
+    cell2 = pair[1]
+
+    matrices = [element['ppis'][cell1][['A', 'B']].values * element['ppis'][cell2][['A', 'B']].values
+                for element in subsampled_interactions]
+    matrices = np.concatenate(matrices, axis=1)
+    return matrices
+
+
 def compute_avg_count_for_ppis(subsampled_interactions, clusters, ppi_dict):
     interaction_type = subsampled_interactions[0]['interaction_type']
     ppi_network = ppi_dict[interaction_type]
@@ -41,11 +51,27 @@ def compute_avg_count_for_ppis(subsampled_interactions, clusters, ppi_dict):
     return mean_matrix, std_matrix
 
 
-def ppi_count_operation(pair, subsampled_interactions):
-    cell1 = pair[0]
-    cell2 = pair[1]
+def get_ppi_score_for_cell_pairs(cells, subsampled_interactions, ppi_dict):
+    interaction_type = subsampled_interactions[0]['interaction_type']
+    ppi_network = ppi_dict[interaction_type]
 
-    matrices = [element['ppis'][cell1][['A', 'B']].values * element['ppis'][cell2][['A', 'B']].values
-                for element in subsampled_interactions]
-    matrices = np.concatenate(matrices, axis=1)
-    return matrices
+    cell_pairs = list(itertools.combinations(cells + cells, 2))
+    columns = ['{}_{}'.format(pair[0], pair[1]) for pair in cell_pairs]
+    index = ppi_network.apply(lambda row: tuple(sorted([row[0], row[1]])), axis=1)
+    mean_matrix = pd.DataFrame(index=index, columns=columns)
+    std_matrix = pd.DataFrame(index=index, columns=columns)
+
+    for pair in cell_pairs:
+        matrices = list(map(functools.partial(ppi_count_operation, subsampled_interactions=subsampled_interactions),
+                            cell_pairs))
+
+        matrices = np.concatenate(matrices, axis=1)
+        mean_column = np.nanmean(matrices, axis=1)
+        std_column = np.nanstd(matrices, axis=1)
+
+        mean_matrix['{}_{}'.format(pair[0], pair[1])] = mean_column
+        std_matrix['{}_{}'.format(pair[0], pair[1])] = std_column
+
+    mean_matrix = mean_matrix.fillna(0.0)
+    std_matrix = std_matrix.fillna(0.0)
+    return mean_matrix, std_matrix
