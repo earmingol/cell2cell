1de50953b723e9e05b08585fcc3a5b1e01c9a22e
earmingol <earmingol14@gmail.com>
2019-10-21 15:57:05 -0700

    Get active PPIs for cell pairs

--- cell2cell/analysis/ppi_enrichment.py
+++ cell2cell/analysis/ppi_enrichment.py
@@ -12,7 +12,7 @@ def ppi_count_operation(pair, subsampled_interactions):
     cell1 = pair[0]
     cell2 = pair[1]
 
-    matrices = [element['ppis'][cell1][['A', 'B']].values * element['ppis'][cell2][['A', 'B']].values
+    matrices = [np.multiply(element['ppis'][cell1][['A']].values, element['ppis'][cell2][['B']].values)
                 for element in subsampled_interactions]
     matrices = np.concatenate(matrices, axis=1)
     return matrices
@@ -43,9 +43,11 @@ def compute_avg_count_for_ppis(subsampled_interactions, clusters, ppi_dict):
         mean_matrix[cluster] = mean_column
         std_matrix[cluster] = std_column
 
+    mean_matrix = mean_matrix.dropna(how='all')
     mean_matrix = mean_matrix.fillna(0.0)
     mean_matrix = mean_matrix.groupby(level=0).sum() / 2.0
 
+    std_matrix = std_matrix.dropna(how='all')
     std_matrix = std_matrix.fillna(0.0)
     std_matrix = std_matrix.groupby(level=0).sum() / 2.0
     return mean_matrix, std_matrix
@@ -56,22 +58,23 @@ def get_ppi_score_for_cell_pairs(cells, subsampled_interactions, ppi_dict):
     ppi_network = ppi_dict[interaction_type]
 
     cell_pairs = list(itertools.combinations(cells + cells, 2))
-    columns = ['{}_{}'.format(pair[0], pair[1]) for pair in cell_pairs]
-    index = ppi_network.apply(lambda row: tuple(sorted([row[0], row[1]])), axis=1)
-    mean_matrix = pd.DataFrame(index=index, columns=columns)
-    std_matrix = pd.DataFrame(index=index, columns=columns)
+    cell_pairs = list(set(cell_pairs))
+    col_labels = ['{};{}'.format(pair[0], pair[1]) for pair in cell_pairs]
+    index = ppi_network.apply(lambda row: (row[0], row[1]), axis=1) # tuple(sorted([row[0], row[1]]))
+    mean_matrix = pd.DataFrame(index=index, columns=col_labels)
+    std_matrix = pd.DataFrame(index=index, columns=col_labels)
 
-    for pair in cell_pairs:
-        matrices = list(map(functools.partial(ppi_count_operation, subsampled_interactions=subsampled_interactions),
-                            cell_pairs))
-
-        matrices = np.concatenate(matrices, axis=1)
+    for i, pair in enumerate(cell_pairs):
+        matrices = ppi_count_operation(pair=pair, subsampled_interactions=subsampled_interactions)
         mean_column = np.nanmean(matrices, axis=1)
         std_column = np.nanstd(matrices, axis=1)
 
-        mean_matrix['{}_{}'.format(pair[0], pair[1])] = mean_column
-        std_matrix['{}_{}'.format(pair[0], pair[1])] = std_column
+        mean_matrix[col_labels[i]] = mean_column
+        std_matrix[col_labels[i]] = std_column
 
+    mean_matrix = mean_matrix.dropna(how='all')
     mean_matrix = mean_matrix.fillna(0.0)
+
+    std_matrix = std_matrix.dropna(how='all')
     std_matrix = std_matrix.fillna(0.0)
     return mean_matrix, std_matrix
