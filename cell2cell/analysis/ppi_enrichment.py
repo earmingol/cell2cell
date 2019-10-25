222c5983374eefa2ede0a845d474b353197d229d
earmingol <earmingol14@gmail.com>
2019-10-25 12:51:39 -0700

    Added reference ppi option to avoid plotting bidirectionality

--- cell2cell/analysis/ppi_enrichment.py
+++ cell2cell/analysis/ppi_enrichment.py
@@ -8,12 +8,16 @@ import numpy as np
 import pandas as pd
 
 
-def ppi_count_operation(pair, subsampled_interactions):
+def ppi_count_operation(pair, subsampled_interactions, index=None):
     cell1 = pair[0]
     cell2 = pair[1]
 
-    matrices = [np.multiply(element['ppis'][cell1][['A']].values, element['ppis'][cell2][['B']].values)
-                for element in subsampled_interactions]
+    if index is None:
+        matrices = [np.multiply(element['ppis'][cell1][['A']].values, element['ppis'][cell2][['B']].values)
+                    for element in subsampled_interactions]
+    else:
+        matrices = [np.multiply(element['ppis'][cell1][['A']].values[index], element['ppis'][cell2][['B']].values[index])
+                    for element in subsampled_interactions]
     matrices = np.concatenate(matrices, axis=1)
     return matrices
 
@@ -50,16 +54,24 @@ def compute_avg_count_for_ppis(subsampled_interactions, clusters, ppi_data):
     return mean_matrix, std_matrix
 
 
-def get_ppi_score_for_cell_pairs(cells, subsampled_interactions, ppi_data):
+def get_ppi_score_for_cell_pairs(cells, subsampled_interactions, ppi_data, ref_ppi_data=None):
     cell_pairs = list(itertools.combinations(cells + cells, 2))
     cell_pairs = list(set(cell_pairs))
     col_labels = ['{};{}'.format(pair[0], pair[1]) for pair in cell_pairs]
-    index = ppi_data.apply(lambda row: (row[0], row[1]), axis=1) # tuple(sorted([row[0], row[1]]))
-    mean_matrix = pd.DataFrame(index=index, columns=col_labels)
-    std_matrix = pd.DataFrame(index=index, columns=col_labels)
+    if ref_ppi_data is None:
+        index = ppi_data.apply(lambda row: (row[0], row[1]), axis=1)
+        keep_index=None
+        mean_matrix = pd.DataFrame(index=index, columns=col_labels)
+        std_matrix = pd.DataFrame(index=index, columns=col_labels)
+    else:
+        ref_index = list(ref_ppi_data.apply(lambda row: (row[0], row[1]), axis=1).values)
+        keep_index = list(pd.merge(ppi_data, ref_ppi_data, how='inner').index)
+        mean_matrix = pd.DataFrame(index=ref_index, columns=col_labels)
+        std_matrix = pd.DataFrame(index=ref_index, columns=col_labels)
+
 
     for i, pair in enumerate(cell_pairs):
-        matrices = ppi_count_operation(pair=pair, subsampled_interactions=subsampled_interactions)
+        matrices = ppi_count_operation(pair=pair, subsampled_interactions=subsampled_interactions, index=keep_index)
         mean_column = np.nanmean(matrices, axis=1)
         std_column = np.nanstd(matrices, axis=1)
 
@@ -69,6 +81,6 @@ def get_ppi_score_for_cell_pairs(cells, subsampled_interactions, ppi_data):
     mean_matrix = mean_matrix.dropna(how='all')
     mean_matrix = mean_matrix.fillna(0.0)
 
-    std_matrix = std_matrix.dropna(how='all')
+    std_matrix = std_matrix.loc[mean_matrix.index, mean_matrix.columns]
     std_matrix = std_matrix.fillna(0.0)
     return mean_matrix, std_matrix
