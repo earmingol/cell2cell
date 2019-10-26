c6a70455d44054f72097a18deed09509ddbe4f29
earmingol <earmingol14@gmail.com>
2019-10-25 17:10:21 -0700

    Added heuristic and ligand-receptor pipelines

--- cell2cell/analysis/pipelines.py
+++ cell2cell/analysis/pipelines.py
@@ -2,15 +2,19 @@
 
 from __future__ import absolute_import
 
-from cell2cell.clustering import cluster_interactions
-from cell2cell.core import subsampling
+import os
+
+from cell2cell.analysis import ppi_enrichment
+from cell2cell.core import interaction_space as ispace
 from cell2cell.datasets import heuristic_data
 from cell2cell.io import read_data
-from cell2cell.preprocessing import integrate_data
+from cell2cell.preprocessing import gene_ontology, integrate_data, ppi
+from cell2cell.utils import plotting
 
 
-def heuristic_pipeline(files, rnaseq_setup, ppi_setup, cutoff_setup, go_setup, analysis_setup,
-                       contact_go_terms = None, mediator_go_terms = None):
+def heuristic_pipeline(files, rnaseq_setup, ppi_setup, meta_setup, cutoff_setup, go_setup, analysis_setup,
+                       contact_go_terms = None, mediator_go_terms = None, interaction_type='combined',
+                       excluded_cells=None, verbose=None):
     '''
     This function performs the analysis with the default list of GO terms to filter the proteins in the PPI network.
 
@@ -57,24 +61,42 @@ def heuristic_pipeline(files, rnaseq_setup, ppi_setup, cutoff_setup, go_setup, a
 
 
     '''
+    if excluded_cells is None:
+        excluded_cells = []
 
+    # Check for output directory
+    if not os.path.exists(files['output_folder']):
+        os.makedirs(files['output_folder'])
+
+    # Load Data
     rnaseq_data = read_data.load_rnaseq(rnaseq_file=files['rnaseq'],
                                         gene_column=rnaseq_setup['gene_col'],
                                         drop_nangenes=rnaseq_setup['drop_nangenes'],
                                         log_transformation=rnaseq_setup['log_transform'],
-                                        format='auto')
+                                        format='auto',
+                                        verbose=verbose
+                                        )
+
+    meta = read_data.load_metadata(metadata_file=files['metadata'],
+                                   rnaseq_data=rnaseq_data,
+                                   sample_col=meta_setup['sample_col'],
+                                   format='auto',
+                                   verbose=verbose
+                                   )
 
     ppi_data = read_data.load_ppi(ppi_file=files['ppi'],
                                   interaction_columns=ppi_setup['protein_cols'],
-                                  score=ppi_setup['score_col'],
                                   rnaseq_genes=list(rnaseq_data.index),
-                                  format='auto')
+                                  format='auto',
+                                  verbose=verbose
+                                  )
 
     go_annotations = read_data.load_go_annotations(goa_file=files['go_annotations'],
-                                                   experimental_evidence=go_setup['goa_experimental_evidence'])
+                                                   experimental_evidence=go_setup['experimental_evidence'])
 
     go_terms = read_data.load_go_terms(go_terms_file=files['go_terms'])
 
+    # Filter genes associated to GO terms
     default_heuristic_go = heuristic_data.HeuristicGOTerms()
 
     if contact_go_terms is None:
@@ -83,46 +105,172 @@ def heuristic_pipeline(files, rnaseq_setup, ppi_setup, cutoff_setup, go_setup, a
     if mediator_go_terms is None:
         mediator_go_terms = default_heuristic_go.mediator_go_terms
 
+    contact_genes = gene_ontology.get_genes_from_go_hierarchy(go_annotations,
+                                                                  go_terms,
+                                                                  contact_go_terms)
+
+    mediator_genes = gene_ontology.get_genes_from_go_hierarchy(go_annotations,
+                                                               go_terms,
+                                                               mediator_go_terms)
+
+    contact_genes = list(set(contact_genes))
+    mediator_genes = list(set(mediator_genes))
+
     # Run analysis
-    ppi_dict = integrate_data.get_ppi_dict_from_go_terms(ppi_data=ppi_data,
-                                                         go_annotations=go_annotations,
-                                                         go_terms=go_terms,
-                                                         contact_go_terms=contact_go_terms,
-                                                         mediator_go_terms=mediator_go_terms,
-                                                         use_children=go_setup['go_descendants'],
-                                                         verbose=False)
+    ppi_dict = integrate_data.get_ppi_dict_from_proteins(ppi_data,
+                                                         contact_genes,
+                                                         mediator_genes,
+                                                         bidirectional=False,
+                                                         verbose=verbose
+                                                         )
 
-    print("Running analysis with a sub-sampling percentage of {}% and {} iterations".format(
-          int(100*analysis_setup['subsampling_percentage']), analysis_setup['iterations'])
-          )
+    bi_ppi_data = ppi.bidirectional_ppi_for_cci(ppi_data=ppi_dict[interaction_type])
 
-    if 'initial_seed' not in analysis_setup.keys():
-        initial_seed = None
-    else:
-        initial_seed = analysis_setup['initial_seed']
+    interaction_space = ispace.InteractionSpace(rnaseq_data=rnaseq_data,
+                                                ppi_data=bi_ppi_data,
+                                                gene_cutoffs=cutoff_setup,
+                                                score_type=analysis_setup['score_type'],
+                                                score_metric=analysis_setup['score_metric'],
+                                                verbose=verbose)
 
-    subsampling_space = subsampling.SubsamplingSpace(rnaseq_data=rnaseq_data,
-                                                     ppi_dict=ppi_dict,
-                                                     interaction_type=analysis_setup['interaction_type'],
-                                                     gene_cutoffs=cutoff_setup,
-                                                     score_type=analysis_setup['function_type'],
-                                                     subsampling_percentage=analysis_setup['subsampling_percentage'],
-                                                     iterations=analysis_setup['iterations'],
-                                                     initial_seed=initial_seed,
-                                                     n_jobs=analysis_setup['cpu_cores'],
-                                                     verbose=analysis_setup['verbose'])
+    interaction_space.compute_pairwise_interactions(verbose=verbose)
+
+    clustermap = plotting.clustermap_cci(interaction_space,
+                                         method='ward',
+                                         excluded_cells=excluded_cells,
+                                         metadata=meta,
+                                         sample_col=meta_setup['sample_col'],
+                                         group_col=meta_setup['group_col'],
+                                         title='CCI scores for cell pairs',
+                                         filename=files['output_folder'] + 'CCI-Clustermap-CCI-scores.png',
+                                         **{'cmap': 'Blues'}
+                                         )
+
+    pcoa = plotting.pcoa_biplot(interaction_space,
+                                excluded_cells=excluded_cells,
+                                metadata=meta,
+                                sample_col=meta_setup['sample_col'],
+                                group_col=meta_setup['group_col'],
+                                title='PCoA for cells given their CCI scores',
+                                filename=files['output_folder'] + 'CCI-PCoA-CCI-scores.png',
+                                )
+
+    interaction_matrix, std_interaction_matrix = ppi_enrichment.get_ppi_score_for_cell_pairs(
+        cells=list(interaction_space.distance_matrix.columns),
+        subsampled_interactions=[interaction_space.interaction_elements],
+        ppi_data=bi_ppi_data,
+        ref_ppi_data=ppi_data
+        )
+
+    interaction_clustermap = plotting.clustermap_cell_pairs_vs_ppi(interaction_matrix,
+                                                                   metric='jaccard',
+                                                                   metadata=meta,
+                                                                   sample_col=meta_setup['sample_col'],
+                                                                   group_col=meta_setup['group_col'],
+                                                                   excluded_cells=excluded_cells,
+                                                                   title='Active ligand-receptor pairs for interacting cells',
+                                                                   filename=files[
+                                                                                'output_folder'] + 'CCI-Active-LR-pairs.png',
+                                                                   **{'figsize': (20, 40)}
+                                                                   )
+
+    interaction_clustermap.data2d.to_csv(files['output_folder'] + 'CCI-Active-LR-pairs.csv')
+
+    outputs = dict()
+    outputs['interaction_space'] = interaction_space
+    outputs['clustermap'] = clustermap
+    outputs['pcoa'] = pcoa
+    outputs['interaction_clustermap'] = interaction_clustermap
+    outputs['LR-pairs'] = interaction_clustermap.data2d
+    return outputs
 
 
-    print("Performing clustering with {} algorithm and {} method".format(analysis_setup['clustering_algorithm'],
-                                                                         analysis_setup['clustering_method']))
+def ligand_receptor_pipeline(files, rnaseq_setup, ppi_setup, meta_setup, cutoff_setup, analysis_setup, excluded_cells=None,
+                             verbose=True):
+    if excluded_cells is None:
+        excluded_cells = []
 
-    clustering = cluster_interactions.get_clusters_from_graph(subsampled_interactions=subsampling_space.subsampled_interactions,
-                                                              algorithm=analysis_setup['clustering_algorithm'],
-                                                              method=analysis_setup['clustering_method'],
-                                                              seed=None,
-                                                              n_jobs=analysis_setup['cpu_cores'],
-                                                              verbose=analysis_setup['verbose'])
+    # Check for output directory
+    if not os.path.exists(files['output_folder']):
+        os.makedirs(files['output_folder'])
 
-    subsampling_space.graph_clustering = clustering
+    # Load Data
+    rnaseq_data = read_data.load_rnaseq(rnaseq_file=files['rnaseq'],
+                                        gene_column=rnaseq_setup['gene_col'],
+                                        drop_nangenes=rnaseq_setup['drop_nangenes'],
+                                        log_transformation=rnaseq_setup['log_transform'],
+                                        format='auto',
+                                        verbose=verbose)
 
-    return subsampling_space, ppi_dict
+    ppi_data = read_data.load_ppi(ppi_file=files['ppi'],
+                                  interaction_columns=ppi_setup['protein_cols'],
+                                  rnaseq_genes=list(rnaseq_data.index),
+                                  format='auto',
+                                  verbose=verbose)
+
+    meta = read_data.load_metadata(metadata_file=files['metadata'],
+                                   rnaseq_data=rnaseq_data,
+                                   sample_col=meta_setup['sample_col'],
+                                   format='auto',
+                                   verbose=verbose)
+
+    # Run Analysis
+    bi_ppi_data = ppi.bidirectional_ppi_for_cci(ppi_data=ppi_data,
+                                                verbose=verbose)
+
+    interaction_space = ispace.InteractionSpace(rnaseq_data=rnaseq_data,
+                                                ppi_data=bi_ppi_data,
+                                                gene_cutoffs=cutoff_setup,
+                                                score_type=analysis_setup['score_type'],
+                                                score_metric=analysis_setup['score_metric'],
+                                                verbose=verbose)
+
+    interaction_space.compute_pairwise_interactions(verbose=verbose)
+
+
+    clustermap = plotting.clustermap_cci(interaction_space,
+                                         method='ward',
+                                         excluded_cells=excluded_cells,
+                                         metadata=meta,
+                                         sample_col=meta_setup['sample_col'],
+                                         group_col=meta_setup['group_col'],
+                                         title='CCI scores for cell pairs',
+                                         filename=files['output_folder'] + 'CCI-Clustermap-CCI-scores.png',
+                                         **{'cmap': 'Blues'}
+                                         )
+
+    pcoa = plotting.pcoa_biplot(interaction_space,
+                                 excluded_cells=excluded_cells,
+                                 metadata=meta,
+                                 sample_col=meta_setup['sample_col'],
+                                 group_col=meta_setup['group_col'],
+                                 title='PCoA for cells given their CCI scores',
+                                 filename=files['output_folder'] + 'CCI-PCoA-CCI-scores.png',
+                                 )
+
+    interaction_matrix, std_interaction_matrix = ppi_enrichment.get_ppi_score_for_cell_pairs(cells=list(interaction_space.distance_matrix.columns),
+                                                                                             subsampled_interactions=[interaction_space.interaction_elements],
+                                                                                             ppi_data=bi_ppi_data,
+                                                                                             ref_ppi_data=ppi_data
+                                                                                             )
+
+    interaction_clustermap = plotting.clustermap_cell_pairs_vs_ppi(interaction_matrix,
+                                                                   metric='jaccard',
+                                                                   metadata=meta,
+                                                                   sample_col=meta_setup['sample_col'],
+                                                                   group_col=meta_setup['group_col'],
+                                                                   excluded_cells=excluded_cells,
+                                                                   title='Active ligand-receptor pairs for interacting cells',
+                                                                   filename=files['output_folder'] + 'CCI-Active-LR-pairs.png',
+                                                                   **{'figsize': (20, 40)}
+                                                                   )
+
+    interaction_clustermap.data2d.to_csv(files['output_folder'] + 'CCI-Active-LR-pairs.csv')
+
+    outputs = dict()
+    outputs['interaction_space'] = interaction_space
+    outputs['clustermap'] = clustermap
+    outputs['pcoa'] = pcoa
+    outputs['interaction_clustermap'] = interaction_clustermap
+    outputs['LR-pairs'] = interaction_clustermap.data2d
+    return outputs
