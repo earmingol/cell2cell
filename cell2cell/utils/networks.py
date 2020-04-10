40a1afc9b4d2e37618c756107635289ead83e14d
Erick Armingol <earmingol14@gmail.com>
2020-04-09 18:43:43 -0700

    Removed iGraph

--- cell2cell/utils/networks.py
+++ cell2cell/utils/networks.py
@@ -3,21 +3,20 @@
 from __future__ import absolute_import
 
 import networkx as nx
-import igraph
-
 
 def generate_network_from_adjacency(adjacency_matrix, package='networkx'):
     if package == 'networkx':
         network = nx.from_pandas_adjacency(adjacency_matrix)
     elif package == 'igraph':
-        A = adjacency_matrix.values
-        network = igraph.Graph.Weighted_Adjacency((A > 0).tolist(), mode=igraph.ADJ_UNDIRECTED)
-
-        # Add edge weights and node labels.
-        network.es['weight'] = A[A.nonzero()]
-        network.vs['label'] = list(adjacency_matrix.columns)
-
-        Warning("iGraph functionalities are not completely implemented yet.")
+        # A = adjacency_matrix.values
+        # network = igraph.Graph.Weighted_Adjacency((A > 0).tolist(), mode=igraph.ADJ_UNDIRECTED)
+        #
+        # # Add edge weights and node labels.
+        # network.es['weight'] = A[A.nonzero()]
+        # network.vs['label'] = list(adjacency_matrix.columns)
+        #
+        # Warning("iGraph functionalities are not completely implemented yet.")
+        raise NotImplementedError("Network using package {} not implemented".format(package))
     else:
         raise NotImplementedError("Network using package {} not implemented".format(package))
     return network
