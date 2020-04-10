3366eb828adb03c8cb5f491712b73fac23983413
Erick Armingol <earmingol14@gmail.com>
2020-04-09 18:16:54 -0700

    Export network into gephi file

--- cell2cell/utils/networks.py
+++ cell2cell/utils/networks.py
@@ -21,3 +21,35 @@ def generate_network_from_adjacency(adjacency_matrix, package='networkx'):
     else:
         raise NotImplementedError("Network using package {} not implemented".format(package))
     return network
+
+
+def export_network_to_gephi(cci_matrix, filename, format='excel', network_type='Undirected'):
+    '''
+    Export a CCI matrix into a spreadsheet readable by Gephi.
+    '''
+    # This allows to pass a network directly or an adjacency matrix
+    if type(cci_matrix) != nx.classes.graph.Graph:
+        cci_network = generate_network_from_adjacency(cci_matrix,
+                                                      package='networkx')
+    else:
+        cci_network = cci_matrix
+
+    gephi_df = nx.to_pandas_edgelist(cci_network)
+    gephi_df = gephi_df.assign(Type=network_type)
+    # When weight is not in the network
+    if ('weight' not in gephi_df.columns):
+        gephi_df = gephi_df.assign(weight=1)
+
+    # Transform column names
+    gephi_df = gephi_df[['source', 'target', 'Type', 'weight']]
+    gephi_df.columns = [c.capitalize() for c in gephi_df.columns]
+
+    # Save with different formats
+    if format == 'excel':
+        gephi_df.to_excel(filename, sheet_name='Edges', index=False)
+    elif format == 'csv':
+        gephi_df.to_csv(filename, sep=',', index=False)
+    elif format == 'tsv':
+        gephi_df.to_csv(filename, sep='\t', index=False)
+    else:
+        raise ValueError("Format not supported.")
