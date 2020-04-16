e5ac1f48ddbb570ccd056fb838ace4d019ca77ff
Erick Armingol <earmingol14@gmail.com>
2020-04-16 13:07:27 -0700

    Added new methods

--- cell2cell/core/__init__.py
+++ cell2cell/core/__init__.py
@@ -2,7 +2,8 @@
 
 from __future__ import absolute_import
 
-from cell2cell.core.cci_scores import (compute_braycurtis_like_cci_score, compute_dot_product_score, compute_jaccard_like_cci_score)
+from cell2cell.core.cci_scores import (compute_braycurtis_like_cci_score, compute_count_score, compute_jaccard_like_cci_score,
+                                       matmul_bray_curtis_like, matmul_count_active, matmul_jaccard_like)
 from cell2cell.core.cell import (Cell, get_cells_from_rnaseq)
 from cell2cell.core.interaction_space import (generate_interaction_elements, InteractionSpace)
-from cell2cell.core.subsampling import (subsampling_operation, SubsamplingSpace)
+from cell2cell.core.spatial_interactions import (SpatialCCI)
