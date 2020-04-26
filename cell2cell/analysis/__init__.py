54b1c8946d2de6bc131aec6ac9747a6941ae8831
Erick Armingol <earmingol14@gmail.com>
2020-04-26 00:59:28 -0700

    Moved functions to study PPI enrichment into Communication Scores in InteractionSpace

--- cell2cell/analysis/__init__.py
+++ cell2cell/analysis/__init__.py
@@ -3,6 +3,5 @@
 from __future__ import absolute_import
 
 from cell2cell.analysis.pipelines import (core_pipeline, heuristic_pipeline, ligand_receptor_pipeline)
-from cell2cell.analysis.ppi_enrichment import (compute_avg_count_for_ppis, ppi_count_operation, get_ppi_score_for_cell_pairs)
 # from cell2cell.analysis.func_enrichment import *
 
