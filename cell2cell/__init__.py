472465c26dfd4853b90b8588bfe6bf887ce371b3
Erick Armingol <earmingol14@gmail.com>
2021-01-14 23:46:30 -0300

    Import plotting

--- cell2cell/__init__.py
+++ cell2cell/__init__.py
@@ -8,6 +8,7 @@ from cell2cell import core
 from cell2cell import datasets
 from cell2cell import external
 from cell2cell import io
+from cell2cell import plotting
 from cell2cell import preprocessing
 from cell2cell import stats
 from cell2cell import utils
