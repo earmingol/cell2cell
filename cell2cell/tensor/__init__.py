e300bb2e5d61f9271679ddc94b7514caaa3fdc4b
Erick Armingol <earmingol14@gmail.com>
2021-04-30 11:54:10 -0700

    Added function to create InteractionTensor from any of the new pipelines (BulkInteraction and SingleCellInteraction)

--- cell2cell/tensor/__init__.py
+++ cell2cell/tensor/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from cell2cell.tensor.tensor import (InteractionTensor, PreBuiltTensor, build_context_ccc_tensor, generate_tensor_metadata)
+from cell2cell.tensor.tensor import (InteractionTensor, PreBuiltTensor, build_context_ccc_tensor, generate_tensor_metadata,
+                                     interactions_to_tensor)
