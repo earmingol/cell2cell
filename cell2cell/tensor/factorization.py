876466daa0e802d1122aeecfdf64dbd3a73a847b
Erick Armingol <earmingol14@gmail.com>
2021-07-15 18:21:19 -0700

    Fixed bug of considering mask as tuple when computing error

--- cell2cell/tensor/factorization.py
+++ cell2cell/tensor/factorization.py
@@ -255,7 +255,7 @@ def _compute_norm_error(tensor, tl_object, mask=None):
             mask_ = tl.tensor(mask, **context)
         else:
             mask_ = mask
-        diff = tl.tensor(np.ones(mask.shape), **context) - mask_,
+        diff = tl.tensor(np.ones(mask.shape), **context) - mask_
         tensor_ = tensor * mask_ + rec_ * (diff)
     else:
         tensor_ = tensor
