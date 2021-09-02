71227f8f8fa4dbb66fecb6dfe0e457f47a83a6e1
Erick Armingol <earmingol14@gmail.com>
2021-09-02 01:14:44 -0700

    Added import to compute_distance for clustering

--- cell2cell/clustering/__init__.py
+++ cell2cell/clustering/__init__.py
@@ -2,4 +2,4 @@
 
 from __future__ import absolute_import
 
-from cell2cell.clustering.cluster_interactions import (compute_linkage, get_clusters_from_linkage)
+from cell2cell.clustering.cluster_interactions import (compute_distance, compute_linkage, get_clusters_from_linkage)
