c69d4c7a22df22d527126777feec5215cf35a085
Erick Armingol <earmingol14@gmail.com>
2021-12-24 21:48:04 -0300

    Implemented UMAP

---
+++ cell2cell/external/umap.py
@@ -0,0 +1,78 @@
+import umap
+
+import pandas as pd
+import scipy.spatial as sp
+
+
+def run_umap(rnaseq_data, axis=1, metric='euclidean', min_dist=0.4, n_neighbors=8, random_state=None, **kwargs):
+    '''Runs UMAP on a expression matrix.
+    Parameters
+    __________
+    rnaseq_data : pandas.DataFrame
+        A dataframe of gene expression values wherein the rows are the genes and
+        columns the cells, tissues or samples.
+
+    axis : int, default=0
+        An axis of the dataframe (0 across rows, 1 across columns).
+        Across rows means that the UMAP is to compare genes, while
+        across columns is to compare cells, tissues or samples.
+
+    metric : str, default='euclidean'
+        The distance metric to use. The distance function can be 'braycurtis',
+        'canberra', 'chebyshev', 'cityblock', 'correlation', 'cosine', 'dice',
+        'euclidean', 'hamming', 'jaccard', 'jensenshannon', 'kulsinski',
+        'mahalanobis', 'matching', 'minkowski', 'rogerstanimoto', 'russellrao',
+        'seuclidean', 'sokalmichener', 'sokalsneath', 'sqeuclidean', 'yule'.
+
+    min_dist: float, default=0.4
+        The effective minimum distance between embedded points. Smaller values
+        will result in a more clustered/clumped embedding where nearby points
+        on the manifold are drawn closer together, while larger values will
+        result on a more even dispersal of points. The value should be set
+        relative to the ``spread`` value, which determines the scale at which
+        embedded points will be spread out.
+
+    n_neighbors: float, default=8
+        The size of local neighborhood (in terms of number of neighboring
+        sample points) used for manifold approximation. Larger values
+        result in more global views of the manifold, while smaller
+        values result in more local data being preserved. In general
+        values should be in the range 2 to 100.
+
+    random_state : int, default=None
+        Seed for randomization.
+
+    **kwargs : dict
+        Extra arguments for UMAP as defined in umap.UMAP.
+
+    Returns
+    -------
+    umap_df : pandas.DataFrame
+        Dataframe containing the UMAP embeddings for the axis analyzed.
+        Contains columns 'umap1 and 'umap2'.
+    '''
+    # Organize data
+    if axis == 0:
+        df = rnaseq_data
+    elif axis == 1:
+        df = rnaseq_data.T
+    else:
+        raise ValueError("The parameter axis must be either 0 or 1.")
+
+    # Compute distances
+    D = sp.distance.pdist(df, metric='euclidean')
+    D_sq = sp.distance.squareform(D)
+
+    # Run UMAP
+    model = umap.UMAP(metric="precomputed",
+                      min_dist=min_dist,
+                      n_neighbors=n_neighbors,
+                      random_state=random_state,
+                      **kwargs
+                      )
+
+    trans_D = model.fit_transform(D_sq)
+
+    # Organize results
+    umap_df = pd.DataFrame(trans_D, columns=['umap1', 'umap2'], index=df.index)
+    return umap_df
---
+++ cell2cell/plotting/umap_plot.py
@@ -0,0 +1,102 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def umap_biplot(umap_df, figsize=(8 ,8), show_axes=True, show_legend=True, hue=None,
+                cmap='tab10', fontsize=20, filename=None):
+    '''Plots a UMAP biplot for the UMAP embeddings.
+
+    Parameters
+    ----------
+    umap_df : pandas.DataFrame
+        Dataframe containing the UMAP embeddings for the axis analyzed.
+        It must contain columns 'umap1 and 'umap2'. If a hue column is
+        provided in the parameter 'hue', that column must be provided
+        in this dataframe.
+
+    figsize : tuple, default=(8, 8)
+        Size of the figure (width*height), each in inches.
+
+    show_axes : boolean, default=True
+        Whether showing lines, ticks and ticklabels of both axes.
+
+    show_legend : boolean, default=True
+        Whether including the legend when a hue is provided.
+
+    hue : vector or key in 'umap_df'
+        Grouping variable that will produce points with different colors.
+        Can be either categorical or numeric, although color mapping will
+        behave differently in latter case.
+
+    cmap : str, default='tab10'
+        Name of the color palette for coloring elements with UMAP embeddings.
+
+    fontsize : int, default=20
+        Fontsize of the axis labels (UMAP1 and UMAP2).
+
+    filename : str, default=None
+        Path to save the figure of the elbow analysis. If None, the figure is not
+        saved.
+
+    Returns
+    -------
+        fig : matplotlib.figure.Figure
+            A matplotlib Figure instance.
+
+        ax : matplotlib.axes.Axes
+            The matplotlib axes containing the plot.
+    '''
+
+    fig = plt.figure(figsize=figsize)
+
+    ax = sns.scatterplot(x='umap1',
+                         y='umap2',
+                         data=umap_df,
+                         hue=hue,
+                         )
+
+    if show_axes:
+        sns.despine(ax=ax,
+                    offset=15
+                    )
+
+        ax.tick_params(axis='both',
+                       which='both',
+                       colors='black',
+                       width=2,
+                       length=5
+                       )
+    else:
+        plt.xticks([])
+        plt.yticks([])
+        for key, spine in ax.spines.items():
+            spine.set_visible(False)
+
+
+    for tick in ax.get_xticklabels():
+        tick.set_fontproperties('arial')
+        tick.set_weight("bold")
+        tick.set_color("black")
+        tick.set_fontsize(int(0.7*fontsize))
+    for tick in ax.get_yticklabels():
+        tick.set_fontproperties('arial')
+        tick.set_weight("bold")
+        tick.set_color("black")
+        tick.set_fontsize(int(0.7*fontsize))
+
+    ax.set_xlabel('UMAP 1', fontsize=fontsize)
+    ax.set_ylabel('UMAP 2', fontsize=fontsize)
+
+    if (show_legend) & (hue is not None):
+        # Put the legend out of the figure
+        legend = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+        legend.set_title(hue)
+        legend.get_title().set_fontsize(int(0.7*fontsize))
+
+        for text in legend.get_texts():
+            text.set_fontsize(int(0.7*fontsize))
+
+    if filename is not None:
+        plt.savefig(filename, dpi=300, bbox_inches='tight')
+
+    return fig, ax
