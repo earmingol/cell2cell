9ec42e0883d385f217c3b917433297894a981b67
Erick Armingol <earmingol14@gmail.com>
2022-02-15 13:56:17 -0800

    License

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # ----------------------------------------------------------------------------
 # Copyright (c) 2019--, Cell2cell development team.
 #
-# Distributed under the terms of the MIT License.
+# Distributed under the terms of the BSD 3-Clause License.
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
