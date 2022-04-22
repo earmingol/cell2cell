0583dd29f74dfd9700f8d69d3777952145f366ac
hmbaghdassarian <hmbaghdassarian@eng.ucsd.edu>
2022-04-22 00:25:44 +0000

    include multiplicative products

--- cell2cell/core/cell_composition.py
+++ cell2cell/core/cell_composition.py
@@ -15,10 +15,13 @@ class CellComposition:
     Some scores are at single-cell resolution, others at the higher-order grouping (e.g., cell type or cluster). 
     """
     
-    _method_map = {'gmean': gmean, 'mean': np.mean, 'median': np.median}
+    _method_map = {'gmean': gmean, 'mean': np.mean, 'median': np.median, 'product': np.product}
     
-    def __init__(self, expr: pd.DataFrame, metadata: pd.DataFrame, manifold: Optional[pd.DataFrame] = None,
-                random_state: Optional[int] = 888):
+    def __init__(self, 
+                 expr: pd.DataFrame, 
+                 metadata: pd.DataFrame, context_label: str = 'Sample_ID', cellgroup_label: str = 'cell_type',
+                 manifold: Optional[pd.DataFrame] = None,
+                 random_state: Optional[int] = 888):
         """Get the cell composition of the data
 
         Parameters
@@ -30,6 +33,10 @@ class CellComposition:
             cell-level metadata, with index as cell barcodes 
             should include the samples/contexts (e.g., disease state or time point) and a higher-level grouping (e.g., cell type or cluster)
             if working with a single context, still include this metadata with a single identifier label
+        context_label : str, optional
+            metadata column that separates cells by sample or context, by default 'Sample_ID'
+        cellgroup_label : str, optional
+            metadata column that separates cells by higher-level cell grouping (e.g., cell type or cluster), by default 'cell_type'
         manifold : pd.DataFrame, optional
             reduced dimension coordinates, recommended PCA, by default None
             index is the cell barcode (same as metadata), columns are each dimension
@@ -37,8 +44,18 @@ class CellComposition:
         random_state : int, optional
             seed for reproducible outputs, by default 888
         """
+        # categorically order cellgroup in metadata
+        if metadata[cellgroup_label].dtype.name != 'category' or not metadata[cellgroup_label].dtype.ordered:
+            metadata[cellgroup_label] = pd.Categorical(metadata[cellgroup_label], ordered = True, 
+                                                       categories = sorted(metadata[cellgroup_label].unique()))
+        
         self.expr = expr
+        
         self.metadata = metadata
+        self.context_label = context_label
+        self.cellgroup_label = cellgroup_label
+        self._cellgroup_order = self.metadata.sort_values(by = self.cellgroup_label, ascending=True)[self.cellgroup_label].unique()
+
         self.manifold = manifold
         self._check_barcodes()
         
@@ -76,48 +93,75 @@ class CellComposition:
         self.manifold = pd.DataFrame(data_pca, 
                                     columns = ['PC_' + str(i) for i in range(1,n_pcs+1)], 
                                     index = self.expr.columns)
+    def _order_aggregates(self):
+        """Categorically order the ._aggregate_composition_score index."""
+        for context, composition_score in self.aggregate_composition_score.items():
+            self.aggregate_composition_score[context] = composition_score.loc[self._cellgroup_order,:]
     
-    def score_composition(self, score_type: str = '', **kwargs):
-        """Get a "composition score" representative of the cell or cell type frequency"""
+    def score_composition(self, score_type: str = 'frequency', **kwargs):
+        """Get a "composition score" representative of the cell or cell type frequency.
+        
+        For methods that are at the single cell resolution, must aggregate using "CellComposition.get_aggregate_composition_score".
+
+        Parameters
+        ----------
+        score_type : str, optional
+            the method to calculate the composition score, by default 'frequency'
+            Options are:
+            - 'frequency' : frequency of cell type calculated separately for each context, aggregate resolution 
+            - 'meld' : likelihood of observing a cell in a given context, single-cell resolution
+        """
+        # aggregated by cell type methods
+        if score_type == 'frequency':
+            self.score_composition_frequency(**kwargs)
+            
+        # single-cell resolution methods
         if score_type == 'meld':
             self.score_composition_meld(**kwargs)
-    
-    def score_composition_meld(self, context_label = 'Sample_ID'):
+
+    def score_composition_frequency(self):
+        """Calculates the composition score as the frequency of a cell type (self.cellgroup_label) in a given context."""
+        # get counts per context
+        cell_counts = pd.DataFrame(self.metadata.groupby(self.context_label)[self.cellgroup_label].value_counts()).reset_index()
+        cell_counts.columns = [self.context_label, self.cellgroup_label, 'counts']
+
+        # get frequency per context
+        cell_counts['Composition_Score'] = cell_counts.groupby(self.context_label).counts.apply(lambda x: x/x.sum()).tolist()
+
+        # add to aggregate composition score
+        self.aggregate_composition_score = {}
+        for context in cell_counts[self.context_label].unique():
+            self.aggregate_composition_score[context] = cell_counts[cell_counts[self.context_label] == context][[self.cellgroup_label, 'Composition_Score']].set_index(self.cellgroup_label)
+        self._order_aggregates()
+        
+    def score_composition_meld(self):
         """Calculates the composition score using MELD (https://doi.org/10.1038/s41587-020-00803-5). 
+        
         MELD provides single-cell resolution of relative likelihood of observing a given cell in one context vs all the other contexts. 
         Here, we run MELD with default parameters for each context vs rest, and retain the likelihood scores of the cell if it is actually present in that context. 
 
         Requires > 1 context. 
-
-        Parameters
-        ----------
-        context_label : str, optional
-            metadata column that separates cells by sample or context, by default 'Sample_ID'
-
-        Returns
-        -------
-        composition_metadata : pd.DataFrame
-            same as input metadata df, but with a column that contains a cell compositional score
         """
-        if self.metadata[context_label].nunique() < 2:
+        if self.metadata[self.context_label].nunique() < 2:
             raise ValueError('MELD can only be used to score composition frequencies if there is > 1 context')
         if self.manifold is None:
             self.run_pca()
 
         meld_op = meld.MELD()
-        sample_densities = meld_op.fit_transform(self.manifold, sample_labels=self.metadata[context_label])
+        sample_densities = meld_op.fit_transform(self.manifold, sample_labels=self.metadata[self.context_label])
         sample_likelihoods = meld.utils.normalize_densities(sample_densities)
         sample_likelihoods.index = self.metadata.index
         # TODO: add some normalization to the likelihoods? as more samples are added, the average likelihood decreases
         # can add this either here or at the end
 
         # retain scores from the actual sample the cell was measured in s.t. each single-cell has 1 likelihood score associated with it rather than n_context likelihood scores
-        context_map = dict(zip(self.metadata.index, self.metadata[context_label]))
+        context_map = dict(zip(self.metadata.index, self.metadata[self.context_label]))
         likelihood_score = {}
         for barcode, context in context_map.items():
             likelihood_score[barcode] = sample_likelihoods.loc[barcode, context]
         self.barcode_composition_score = likelihood_score
     
+
 #     def normalize_composition_score(self):
 #         """Normalize the composition score between [0,1]"""
 #         if min(self.barcode_composition_score.values()) < 0:
@@ -126,8 +170,7 @@ class CellComposition:
 #             print('Internal: need to write an appropriate normalization function')
 #             raise ValueError('incomplete code')
 
-    def get_aggregate_composition_score(self, aggregation_method='median', fill_na: float = 0, 
-                                            context_label = 'Sample_ID', cellgroup_label='cell_type'):
+    def get_aggregate_composition_score(self, aggregation_method='median', fill_na: float = 0):
         """Aggregate the composition score by the context and higher-level cell grouping (e.g., cell type or cluster)
 
         Parameters
@@ -142,15 +185,6 @@ class CellComposition:
             value to fill NaN with after aggregation, by default 0
             will not fill if fill_na = None
             NaN arises due to a cell type not being scored as present in a context, which is why we default it to 0 composition 
-        context_label : str, optional
-            metadata column that separates cells by sample or context, by default 'Sample_ID'
-        cellgroup_label : str, optional
-            metadata column that separates cells by higher-level cell grouping (e.g., cell type or cluster), by default 'cell_type'
-
-        Raises
-        ------
-        ValueError
-            _description_
         """
         if self.aggregate_composition_score is not None:
             warnings.warn('An aggregate composition score was already calculated, overwriting')
@@ -160,21 +194,22 @@ class CellComposition:
         composition_metadata = self.metadata.copy()
         composition_metadata['Composition_Score'] = pd.Series(composition_metadata.index).map(self.barcode_composition_score).tolist()
         
-        composition_metadata = composition_metadata.groupby([context_label, cellgroup_label]).Composition_Score.apply(self._method_map[aggregation_method])
+        composition_metadata = composition_metadata.groupby([self.context_label, self.cellgroup_label]).Composition_Score.apply(self._method_map[aggregation_method])
         composition_metadata = pd.DataFrame(composition_metadata).reset_index()
         
         if fill_na is not None:
             composition_metadata.Composition_Score.fillna(value = fill_na, inplace = True)
 
         self.aggregate_composition_score = {}
-        for context in composition_metadata[context_label].unique():
+        for context in composition_metadata[self.context_label].unique():
             self.aggregate_composition_score[context] = \
-                        composition_metadata[composition_metadata[context_label] == context]\
-                        [[cellgroup_label, 'Composition_Score']].reset_index(drop = True).set_index(cellgroup_label)
+                        composition_metadata[composition_metadata[self.context_label] == context]\
+                        [[self.cellgroup_label, 'Composition_Score']].reset_index(drop = True).set_index(self.cellgroup_label)
+        self._order_aggregates()
     @staticmethod
     def get_pairwise_composition_score(composition_score: pd.DataFrame, method: str ='gmean'):
         """Calculate a pairwise composition score between two cell types from their individual composition scores.
-
+    
         Parameters
         ----------
         composition_score : pd.DataFrame
@@ -186,13 +221,14 @@ class CellComposition:
             - 'gmean' : Geometric mean composition score between the cell types
             - 'median' : Median composition score between the cell types
             - 'mean' : Mean composition score between the cell types
+            - 'product': Multiplicative product of composition scores between the cell types
 
         Returns
         -------
         pairwise_score: pd.DataFrame
             the pairwise composition scores
         """
-        
+        # internal: staticmethod to allow user provided inputs
         pairwise_score = pd.DataFrame(CellComposition._method_map[method](np.meshgrid(composition_score, composition_score), axis = 0).flatten(), 
                             columns = ['Composition_Score'], 
                              index = ['-'.join(x) for x in itertools.product(composition_score.index, repeat = 2)])
@@ -201,7 +237,7 @@ class CellComposition:
 def normalize_lr_scores_by_max(lr_communication_scores: Dict[str, pd.DataFrame]):
     """Normalize the LR communication scores to the maximum one across all contexts. 
     This is done to create equal scaling of scores b/w LR communication and cell composition scores
-    Only need if going to use the "scale_by_composition" function. 
+    Only need if going to use the "scale_communication_by_composition" function. 
 
     Parameters
     ----------
@@ -215,6 +251,7 @@ def normalize_lr_scores_by_max(lr_communication_scores: Dict[str, pd.DataFrame])
     lr_communication_scores
         same as input, but with normalized values
     """
+    #TODO: is this the best normalization approach?
     max_score = 0
     for lcs in lr_communication_scores.values():
         if lcs.min().min() < 0:
@@ -230,10 +267,10 @@ def normalize_lr_scores_by_max(lr_communication_scores: Dict[str, pd.DataFrame])
 
     return lr_communication_scores
 
-def scale_by_composition(lr_communication_score: pd.DataFrame, 
-                         pairwise_composition_score: pd.DataFrame, 
+def scale_communication_by_composition(lr_communication_score: pd.DataFrame, 
+                         pairwise_composition_score: pd.DataFrame, method: str = 'weighted_average',
                         composition_weight: float = 0.25):
-    """Scales a LR communication score by the respective pairwise communication scores
+    """Scales a LR communication score by the respective pairwise composition scores
 
     Parameters
     ----------
@@ -244,8 +281,13 @@ def scale_by_composition(lr_communication_score: pd.DataFrame,
         the composition scores between pairs of cells
         index is the (Sender-Receiver) pair that should match the columns of the lr_communication_score
         see CellComposition.get_pairwise_composition_score for details
+    method : str, optional
+        the method by which to combine the LR communication scores with pairwise composition scores, by default "weighted_average"
+        Options are:
+        - 'weighted_average' : Weighted average between two score types
+        - 'product': Product of the two score types
     composition_weight : float, optional
-        weighting of composition score relative to communication score
+        weighting of composition score relative to communication score. only for use when method = 'weighted_average'.
 
     Returns
     -------
@@ -269,10 +311,13 @@ def scale_by_composition(lr_communication_score: pd.DataFrame,
     
 
     # do the calculation
-    communication_score = pd.DataFrame(composition_weight*pairwise_composition_score.values + \
-                                   ((1-composition_weight)*lr_communication_score).T.values, 
-                                  index = lr_communication_score.columns, 
-                                  columns = lr_communication_score.index).T/2
+    if method == 'weighted_average': 
+        communication_score = pd.DataFrame(composition_weight*pairwise_composition_score.values + \
+                                       ((1-composition_weight)*lr_communication_score).T.values, 
+                                      index = lr_communication_score.columns, 
+                                      columns = lr_communication_score.index).T
+    elif method == 'product':
+        communication_score = lr_communication_score.T.mul(pairwise_composition_score.values, axis = 0)
+        
     
     return communication_score
-    
