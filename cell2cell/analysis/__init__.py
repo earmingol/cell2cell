6183ec230a8546d980578c90c4c1183ab472f5f2
Erick Armingol <earmingol14@gmail.com>
2022-11-17 23:33:06 -0800

    Implemented pipeline for Tensor-cell2cell

--- cell2cell/analysis/__init__.py
+++ cell2cell/analysis/__init__.py
@@ -2,6 +2,7 @@
 
 from __future__ import absolute_import
 
-from cell2cell.analysis.pipelines import (initialize_interaction_space, BulkInteractions, SingleCellInteractions)
+from cell2cell.analysis.cell2cell_pipelines import (initialize_interaction_space, BulkInteractions, SingleCellInteractions)
+from cell2cell.analysis.tensor_pipelines import (run_tensor_cell2cell_pipeline)
 import cell2cell.analysis.tensor_downstream as tensor_downstream
 
