f1e571322f83139c089aa3e1747430729e897984
Erick Armingol <earmingol14@gmail.com>
2022-12-13 23:46:11 -0300

    Added other backends that support multiple devices

--- cell2cell/tensor/tensor.py
+++ cell2cell/tensor/tensor.py
@@ -740,7 +740,7 @@ class InteractionTensor(BaseTensor):
                   group for a given pair of cells/tissues/samples
 
     device : str, default=None
-        Device to use when backend is pytorch. Options are:
+        Device to use when backend allows using multiple devices. Options are:
          {'cpu', 'cuda:0', None}
 
     verbose : boolean, default=False
@@ -814,7 +814,7 @@ class InteractionTensor(BaseTensor):
             self.loc_zeros = tl.tensor(self.loc_zeros)
             self.mask = mask
         else:
-            if tl.get_backend() == 'pytorch': # Potential TODO: Include other backends that support different devices
+            if tl.get_backend() in ['pytorch', 'tensorflow']: # Potential TODO: Include other backends that support different devices
                 self.tensor = tl.tensor(tensor, device=device)
                 self.loc_nans = tl.tensor(self.loc_nans, device=device)
                 self.loc_zeros = tl.tensor(self.loc_zeros, device=device)
@@ -866,7 +866,7 @@ class PreBuiltTensor(BaseTensor):
         location of the NaN values.
 
     device : str, default=None
-        Device to use when backend is pytorch. Options are:
+        Device to use when backend allows using multiple devices. Options are:
          {'cpu', 'cuda:0', None}
     '''
     def __init__(self, tensor, order_names, order_labels=None, mask=None, loc_nans=None, device=None):
