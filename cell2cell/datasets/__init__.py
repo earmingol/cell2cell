5df02bfbc0c5172b54cfed5afc2918f4df6cc09c
Erick Armingol <earmingol14@gmail.com>
2023-01-13 17:58:47 -0800

    Minor changes to the documentation

--- cell2cell/datasets/__init__.py
+++ cell2cell/datasets/__init__.py
@@ -2,6 +2,7 @@
 
 from __future__ import absolute_import
 
+from cell2cell.datasets.anndata import (balf_covid)
 from cell2cell.datasets.heuristic_data import (HeuristicGOTerms)
 from cell2cell.datasets.random_data import (generate_random_rnaseq, generate_random_ppi, generate_random_cci_scores,
                                             generate_random_metadata)
