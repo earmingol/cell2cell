e3c5fb698704ef0862df2c4682ff2d4f5b3e4f2d
Erick Armingol <earmingol14@gmail.com>
2023-01-18 18:01:00 -0800

    Added new functions

--- cell2cell/io/__init__.py
+++ cell2cell/io/__init__.py
@@ -2,6 +2,8 @@
 
 from __future__ import absolute_import
 
+from cell2cell.io.directories import (create_directory, get_files_from_directory)
 from cell2cell.io.read_data import (load_cutoffs, load_go_annotations, load_go_terms, load_metadata, load_ppi,
-                                    load_rnaseq, load_table, load_variable_with_pickle, load_tensor_factors)
+                                    load_rnaseq, load_table, load_tables_from_directory, load_variable_with_pickle,
+                                    load_tensor, load_tensor_factors)
 from cell2cell.io.save_data import (export_variable_with_pickle)
